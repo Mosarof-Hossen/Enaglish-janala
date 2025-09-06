b2d046998c38cecfe352d989dd1fa690565b179a
Mosarof-Hossen <mmd58553@gmail.com>
2025-09-06 08:59:57 +0600

    card lesson click and card show added

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const loadlesson = () => {
     .then((json) => displayLesson(json.data));
 };
 
+
+
+
 const displayLesson = (lessons) => {
   //  console.log(lesson);
   // 1. get the container& empty
@@ -17,7 +20,7 @@ const displayLesson = (lessons) => {
     //     3.create Elements
 
     const btnDiv = document.createElement("div");
-    btnDiv.innerHTML = ` <button  class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> Lesson  - ${lesson.level_no} </button>  `;
+    btnDiv.innerHTML = ` <button  onclick="loadLevelWord(${lesson.level_no})"  class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> Lesson  - ${lesson.level_no} </button>  `;
     //     4.append into container
 
     btnLevel.append(btnDiv);
@@ -52,5 +55,39 @@ loadlesson();
 //     btnLevel.append(btnDiv);
 //   }
 // };
+
+
+
+const loadLevelWord= (id)=>{
+  // console.log(id)
+  const url=`https://openapi.programming-hero.com/api/level/${id}`
+fetch(url)
+.then(res=>res.json())
+.then((data)=> displayLevelWord(data.data))
+}
+ 
+const displayLevelWord= (words)=>{
+  // console.log(words)
+  const wordContainer=document.getElementById("word-container");
+  wordContainer.innerHTML="";
+  words.forEach(word => {
+    console.log(word);
+    const card= document.createElement("div");
+    card.innerHTML=`
+    <div class="bg-white px-5 py-10 shadow-sm  rounded-xl text-center space-y-5">
+              <h1 class="text-3xl font-bold">Eager</h1>
+              <p class="text-xl">Meaning /Pronounciation</p>
+              <h1 class="text-3xl font-semibold text-gray-500 bangla-font">পুষ্টি যোগানো / নরিশ"</h1>
+              <div class="flex justify-between items-center">
+                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF90]"><i class="fa-solid fa-circle-info"></i></button>
+                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF90]"><i class="fa-solid fa-volume-high"></i></button>
+                
+              </div>
+            </div>
+    `;
+
+    wordContainer.append(card)
+  });
+}
 
 
